6bec7a0db403b69db91e5f3d3a4eaa4a5de1de08
Luka Zecevic <lz20201092@student.fon.bg.ac.rs>
2025-10-03 21:33:27 +0200

    Promenjen layout stranice

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,33 @@
-const MainLayout = () => {
-  return (
-    <div>MainLayout</div>
-  )
-}
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
+import { GlobalProvider } from '@/context/GlobalContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '@/assets/styles/globals.css';
+import 'photoswipe/dist/photoswipe.css';
 
-export default MainLayout
+export const metadata = {
+  title: 'NextHome',
+  description: 'Find The Perfect Rental Property',
+  keywords: 'rental, property, real estate',
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <AuthProvider>
+      <GlobalProvider>
+        <html lang='en'>
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </GlobalProvider>
+    </AuthProvider>
+  );
+};
+
+export default MainLayout;
